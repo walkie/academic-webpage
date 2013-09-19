0c9376b0ce378d80eae12cdee7765224ed4e8556
Eric Walkingshaw <eric@walkingshaw.net>
2013-09-19 02:47:36 +0200

    Overhaul pub representation.

--- haskell/CV/Paper.hs
+++ haskell/CV/Paper.hs
@@ -7,99 +7,81 @@ type Title = String
 type URL   = String
 type Year  = Int
 
-data Paper
-  = Paper {
-      key     :: Key,
-      authors :: [Author],
-      title   :: Title,
-      year    :: Year,
-      venue   :: Maybe Venue,
-      pages   :: Maybe Pages,
-      url     :: Maybe URL,
-      note    :: Maybe String
-    }
-  deriving (Eq,Show)
+data Kind = Journal | Chapter | Conference | Workshop | Thesis
+          | Report | Consortium | DraftPaper
+  deriving (Eq,Enum,Show)
 
-data Author
-  = Author {
-      firstName :: Name,
-      lastName  :: Name
-    }
-  deriving (Eq,Show)
+data Status = Appeared | Accepted | Submitted | Draft
+  deriving (Eq,Enum,Show)
 
-data Venue
-  = Venue {
-      venueName :: Name,
-      venueKind :: VenueKind
-    }
-  deriving (Eq,Show)
+data Author = Author {
+  _firstName :: Name,
+  _lastName  :: Name
+} deriving (Eq,Show)
 
-data VenueKind 
-  = Conference {
-      conferenceAbbr   :: Name,
-      conferenceSeries :: Maybe (Name,Int)
-    }
-  | Workshop {
-      workshopAbbr :: Name
-    }
-  | Journal {
-      journalAbbr    :: Name,
-      journalVolume  :: Maybe Int,
-      journalNumber  :: Maybe Int
-    }
-  | Chapter {
-      bookEditors   :: [Author],
-      bookPublisher :: Name
-    }
-  | PhdThesis
-  | MsThesis
-  deriving (Eq,Show)
-
-data Pages
-  = Pages   Int Int
-  | PagesIn Int Int Int
+data Pages = Pages   Int Int
+           | PagesIn Int Int Int
   deriving (Eq,Show)
 
 
--- ** Smart constructors
+-- ** Papers
 
--- | A paper with the minimum properties set.
-paper :: Key -> [Author] -> Title -> Year -> Paper
-paper k as t y = Paper k as t y Nothing Nothing Nothing Nothing
+data Paper = Paper {
+  _status  :: Status,
+  _kind    :: Kind,
+  _key     :: Key,
+  _authors :: [Author],
+  _title   :: Title,
+  _year    :: Year,
+  _venue   :: Maybe Venue,
+  _pages   :: Maybe Pages,
+  _url     :: Maybe URL,
+  _note    :: Maybe String
+} deriving (Eq,Show)
 
--- | Set the venue of a paper.
-atVenue :: Paper -> Venue -> Paper
-atVenue p v = p { venue = Just v }
+-- Minimum definition.
+paper s k e a t y = Paper s k e a t y Nothing Nothing Nothing Nothing
 
--- | Set the pages of a paper.
-onPages :: Paper -> Pages -> Paper
-onPages p pp = p { pages = Just pp }
+draft     = paper Draft DraftPaper 
+appeared  = paper Appeared
+accepted  = paper Accepted
+submitted = paper Submitted
 
--- | Set the URL of a paper.
-atURL :: Paper -> URL -> Paper
-atURL p u = p { url = Just u }
+journal  = appeared Journal
+chapter  = appeared Chapter
+conf     = appeared Conference
+workshop = appeared Workshop
+thesis   = appeared Thesis
 
--- | Set the note of a paper.
-withNote :: Paper -> String -> Paper
-withNote p n = p { note = Just n }
 
--- | Draft paper.
-draft :: Key -> [Author] -> Title -> Year -> Paper
-draft k a t y = paper k a t y `withNote` "Draft paper"
+-- Optional field setters.
+infix 3 @@
+p @@ a       = p { _venue = Just a }
+onPages  p a = p { _pages = Just a }
+atURL    p a = p { _url   = Just a }
+withNote p a = p { _note  = Just a }
 
--- | Paper under review.
-submitted :: Key -> [Author] -> Title -> Year -> Venue -> Paper
-submitted k a t y v = paper k a t y `atVenue` v `withNote` "Under review"
 
--- | Accepted but not yet appeared.
-accepted :: String -> [Author] -> Title -> Year -> Venue -> Paper
-accepted k a t y v = paper k a t y `atVenue` v `withNote` "To appear"
+-- ** Venues
 
--- | Paper that has already appeared, with no page numbers.
-appearedNoPages :: String -> [Author] -> Title -> Year -> Venue -> Paper
-appearedNoPages k a t y v = paper k a t y `atVenue` v
+data Venue = Venue {
+  _longName  :: Name,
+  _shortName :: Maybe Name,
+  _publisher :: Maybe Name,
+  _editors   :: Maybe [Author],
+  _volume    :: Maybe Int,
+  _number    :: Maybe Int,
+  _series    :: Maybe (Name,Int)
+} deriving (Eq,Show)
 
--- | Paper that has already appeared.
-appeared :: String -> [Author] -> Title -> Year -> Venue -> Pages -> Paper
-appeared k a t y v p = appearedNoPages k a t y v `onPages` p
+-- Minimum definition.
+venue l = Venue l Nothing Nothing Nothing Nothing Nothing Nothing
+short s l = venue l `setShortName` s
 
+-- Optional field setters.
+setShortName v a = v { _shortName = Just a }
+setPublisher v a = v { _publisher = Just a }
+setEditor    v a = v { _editors   = Just a }
+setVolume    v a = v { _volume    = Just a }
+setNumber    v a = v { _number    = Just a }
+setSeries    v a = v { _series    = Just a }
--- haskell/CV/PaperDB.hs
+++ haskell/CV/PaperDB.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE TupleSections #-}
+{-# LANGUAGE PatternGuards, TupleSections #-}
 
 module CV.PaperDB where
 
@@ -10,57 +10,58 @@ import CV.Paper
 --
 
 -- ** Authors
-me        = Author "Eric" "Walkingshaw"
-martin    = Author "Martin" "Erwig"
-klaus     = Author "Klaus" "Ostermann"
-sheng     = Author "Sheng" "Chen"
-tillmann  = Author "Tillmann" "Rendel"
-duc       = Author "Duc" "Le"
-christian = Author "Christian" "Kästner"
-sven      = Author "Sven" "Apel"
-bodden    = Author "Eric" "Bodden"
-
+walkingshaw = Author "Eric" "Walkingshaw"
+erwig       = Author "Martin" "Erwig"
+cheng       = Author "Sheng" "Chen"
+le          = Author "Duc" "Le"
+ostermann   = Author "Klaus" "Ostermann"
+tillmann    = Author "Tillmann" "Rendel"
+kaestner    = Author "Christian" "Kästner"
+apel        = Author "Sven" "Apel"
+bodden      = Author "Eric" "Bodden"
 
 -- ** Institutions
 osu = "Oregon State University"
 
 -- ** Journals
-journal abbr name vol num = Venue name (Journal abbr vol num)
-
-jfp    = journal "JFP" "Journal of Functional Programming"
-jvlc   = journal "JVLC" "Journal of Visual Languages and Computing"
-toplas = journal "TOPLAS" "ACM Trans. on Programming Languages and Systems"
-tosem  = journal "TOSEM" "ACM Trans. on Software Engineering and Methodology"
+jfp    = short "JFP" "Journal of Functional Programming"
+jvlc   = short "JVLC" "Journal of Visual Languages and Computing"
+toplas = short "TOPLAS" "ACM Trans. on Programming Languages and Systems"
+tosem  = short "TOSEM" "ACM Trans. on Software Engineering and Methodology"
 
 -- ** Conferences
-conf  abbr name = Venue name (Conference abbr Nothing)
-confS abbr name series = Venue name (Conference abbr (Just series))
 lncs = ("LNCS",)
 
-dsl   = Venue "DSL" . Conference "IFIP Working Conf. on Domain-Specific Languages"
-detc  = conf "DETC" "ASME Int. Design Engineering Technical Conf. & Computers and Information in Engineering Conf."
-gpce  = conf "GPCE" "Int. Conf. on Generative Programming and Component Engineering"
-gttse = conf "GTTSE" "Generative and Transformational Techniques in Software Engineering"
-icfp  = conf "ICFP" "ACM SIGPLAN Int. Conf. on Functional Programming"
-sle   = confS "SLE" "Int. Conf. on Software Language Engineering"
-vlhcc = conf "VL/HCC" "IEEE Int. Symp. on Visual Languages and Human-Centric Computing"
-vlhccdc = conf (abbr ++ "-DC") ("Doctoral Consortium at " ++ name)
-  where Venue name (Conference abbr _) = vlhcc
+dsl   = short "DSL" "IFIP Working Conf. on Domain-Specific Languages"
+detc  = short "DETC" "ASME Int. Design Engineering Technical Conf. & Computers and Information in Engineering Conf."
+gpce  = short "GPCE" "Int. Conf. on Generative Programming and Component Engineering"
+gttse = short "GTTSE" "Generative and Transformational Techniques in Software Engineering"
+icfp  = short "ICFP" "ACM SIGPLAN Int. Conf. on Functional Programming"
+sle   = short "SLE" "Int. Conf. on Software Language Engineering"
+vlhcc = short "VL/HCC" "IEEE Int. Symp. on Visual Languages and Human-Centric Computing"
+vlhccdc | Just s <- _shortName vlhcc = short s ("Doctoral Consortium at " ++ _longName vlhcc)
 
 -- ** Workshops
-work abbr name = Venue name (Workshop abbr)
-fosd  = work "FOSD" "Int. Workshop on Feature-Oriented Software Development"
-foser = work "FoSER" "ACM SIGSOFT Workshop on the Future of Software Engineering Research"
-vamos = work "VaMoS" "Int. Workshop on Variability Modelling of Software-Intensive Systems"
+fosd  = short "FOSD" "Int. Workshop on Feature-Oriented Software Development"
+foser = short "FoSER" "ACM SIGSOFT Workshop on the Future of Software Engineering Research"
+vamos = short "VaMoS" "Int. Workshop on Variability Modelling of Software-Intensive Systems"
 
 
 --
 -- * Papers
 --
 
--- ** Lists of papers by kind.
+-- ** Lists of papers in chronological order.
 
 drafts = [v]
+y13 = [fosd13,phdthesis,toplas13,jvlc13,vamos13]
+y12 = [gpce12,icfp12,chapter12]
+y11 = [msthesis,gttse11,vlhcc11,dsl11,sle11,tosem11]
+y10 = [foser10,vlhcc10,vlhcc10dc,qual]
+y09 = [vlhcc09,detc09,jfp09,dsl09a,dsl09b]
+y08 = [vlhcc08,vlhcc08dc]
+
+pubs = concat [y13,y12,y11,y10,y09,y08]
 
 journals  = [toplas13,jvlc13,tosem11,jfp09]
 chapters  = [chapter12,gttse11]
@@ -74,225 +75,224 @@ thesis    = [phdthesis,msthesis,qual]
 
 v = draft
   "XX-variational-data"
-  [me,christian,martin,sven,bodden]
+  [walkingshaw,kaestner,erwig,apel,bodden]
   "Variational Data Structures: Exploring Trade-Offs in Computing With Variability"
   2014
 
-
 -- ** 2013
 
-fosd13 = accepted
+fosd13 = accepted Workshop
   "fosd13-variational-graphs"
-  [martin,me,sheng]
+  [erwig,walkingshaw,cheng]
   "An Abstract Representation of Variational Graphs"
   2013
-  gpce
+  @@ gpce
 
-phdthesis = appearedNoPages
+phdthesis = appeared Thesis
   "thesis-choice-calculus"
-  [me]
+  [walkingshaw]
   "The Choice Calculus: A Formal Language of Variation"
   2013
-  (Venue osu PhdThesis)
   `atURL` "http://hdl.handle.net/1957/40652"
+  @@ venue ("PhD thesis. " ++ osu)
 
-toplas13 = accepted
+toplas13 = accepted Journal
   "toplas13-variational-typing"
-  [sheng,martin,me]
+  [cheng,erwig,walkingshaw]
   "Extending Type Inference to Variational Programs"
   2013
-  (toplas Nothing Nothing)
+  @@ toplas
 
-jvlc13 = appeared
+jvlc13 = appeared Journal
   "jvlc13-probula"
-  [martin,me]
+  [erwig,walkingshaw]
   "A Visual Language for Explaining Probabilistic Reasoning"
   2013
-  (jvlc (Just 24) (Just 2))
-  (Pages 88 109)
+  `onPages` Pages 88 109
+  @@ jvlc `setVolume` 24 `setNumber` 2
 
-vamos13 = appeared
+vamos13 = appeared Workshop
   "vamos13-cc-select"
-  [martin,klaus,tillmann,me]
+  [erwig,ostermann,tillmann,walkingshaw]
   "Adding Configuration to the Choice Calculus"
   2013
-  vamos
-  (PagesIn 13 1 8)
+  `onPages` PagesIn 13 1 8
+  @@ vamos
 
 
 -- ** 2012
 
-gpce12 = appeared
+gpce12 = appeared Conference
   "gpce12-compositional-cc"
-  [me,martin]
+  [walkingshaw,erwig]
   "A Calculus for Modeling and Implementing Variation"
   2012
-  gpce
-  (Pages 132 140)
+  `onPages` Pages 132 140
+  @@ gpce
 
-icfp12 = appeared
+icfp12 = appeared Conference
   "icfp12-variational-type-errors"
-  [sheng,martin,me]
+  [cheng,erwig,walkingshaw]
   "An Error-Tolerant Type System for Variational Lambda Calculus"
   2012
-  icfp
-  (Pages 29 40)
+  `onPages` Pages 29 40
+  @@ icfp
 
-chapter12 = appeared
+chapter12 = appeared Chapter
   "semantics-driven-dsl-design"
-  [martin,me]
+  [erwig,walkingshaw]
   "Semantics-Driven DSL Design"
   2012
-  (Venue "Formal and Practical Aspects of Domain-Specific Languages: Recent Developments"
-    (Chapter [Author "Marjan" "Mernik"] "IGI Global"))
-  (Pages 56 80)
+  `onPages` Pages 56 80
+  @@ venue "Formal and Practical Aspects of Domain-Specific Languages: Recent Developments"
+     `setEditor` [Author "Marjan" "Mernik"]
+     `setPublisher` "IGI Global"
 
 
 -- ** 2011
 
-msthesis = appearedNoPages
+msthesis = appeared Thesis
   "msthesis-game-theory-dsls"
-  [me]
+  [walkingshaw]
   "Domain-Specific Language Support for Experimental Game Theory"
   2011
-  (Venue osu PhdThesis)
   `atURL` "http://hdl.handle.net/1957/26757"
+  @@ venue ("MS thesis. " ++ osu)
 
-gttse11 = appeared
+gttse11 = appeared Chapter
   "gttse11-variation-programming"
-  [martin,me]
+  [erwig,walkingshaw]
   "Variation Programming with the Choice Calculus"
   2012
-  gttse
-  (Pages 55 99)
+  `onPages` Pages 55 99
+  @@ gttse
 
-vlhcc11 = appeared
+vlhcc11 = appeared Conference
   "vlhcc11-ifdef-confirmed-harmful"
-  [duc,me,martin]
+  [le,walkingshaw,erwig]
   "#ifdef Confirmed Harmful: Promoting Understandable Software Variation"
   2011
-  vlhcc
-  (Pages 143 150)
+  `onPages` Pages 143 150
+  @@ vlhcc
 
-dsl11 = appeared
+dsl11 = appeared Conference
   "dsl11-causation-dsl"
-  [me,martin]
+  [walkingshaw,erwig]
   "A DSEL for Studying and Explaining Causation"
   2011
-  (dsl Nothing)
-  (Pages 143 167)
+  `onPages` Pages 143 167
+  @@ dsl
 
-sle11 = appeared
+sle11 = appeared Conference
   "sle11-semantics-first"
-  [martin,me]
+  [erwig,walkingshaw]
   "Semantics First! Rethinking the Language Design Process"
   2011
-  (sle (lncs 6940))
-  (Pages 243 262)
+  `onPages` Pages 243 262
+  @@ sle `setSeries` lncs 6940
 
-tosem11 = appeared
+tosem11 = appeared Journal
   "tosem11-choice-calculus"
-  [martin,me]
+  [erwig,walkingshaw]
   "The Choice Calculus: A Representation for Software Variation"
   2011
-  (tosem (Just 21) (Just 1))
-  (PagesIn 6 1 27)
+  `onPages` PagesIn 6 1 27
+  @@ tosem `setVolume` 21 `setNumber` 1
 
 
 -- ** 2010
 
-foser10 = appeared
+foser10 = appeared Workshop
   "foser10-program-fields"
-  [martin,me]
+  [erwig,walkingshaw]
   "Program Fields for Continuous Software"
   2010
-  foser
-  (Pages 105 108)
+  `onPages` Pages 105 108
+  @@ foser
 
-vlhcc10 = appeared
+vlhcc10 = appeared Conference
   "vlhcc10-neuron-diagrams"
-  [martin,me]
+  [erwig,walkingshaw]
   "Causal Reasoning with Neuron Diagrams"
   2010
-  vlhcc
-  (Pages 101 108)
+  `onPages` Pages 101 108
+  @@ vlhcc
 
-vlhcc10dc = appeared
+vlhcc10dc = appeared Consortium
   "vlhcc10dc-variational-explanations"
-  [me]
+  [walkingshaw]
   "Managing Variation in Explanation-Oriented Languages"
   2010
-  vlhccdc
-  (Pages 247 248)
+  `onPages` Pages 247 248
+  @@ vlhccdc
 
-qual = appearedNoPages
+qual = appeared Thesis
   "qualifier-variation-survey"
-  [me]
+  [walkingshaw]
   "Features and Feature Models: A Survey of Variation Representations"
   2010
-  (Venue "Compendium of Computer Science Doctoral Qualifying Exams"
-    (Chapter [] osu))
   `atURL` "http://hdl.handle.net/1957/19243"
+  @@ venue ("Compendium of Computer Science Doctoral Qualifying Exams. " ++ osu)
 
 
 -- ** 2009
 
-vlhcc09 = appeared
+vlhcc09 = appeared Conference
   "vlhcc09-visual-explanations-probability"
-  [martin,me]
+  [erwig,walkingshaw]
   "Visual Explanations of Probabilistic Reasoning"
   2009
-  vlhcc
-  (Pages 23 27)
+  `onPages` Pages 23 27
+  @@ vlhcc
 
-detc09 = appeared
+detc09 = appeared Conference
   "detc09-software-hardware-design"
-  [me, Author "Paul" "Strauss", martin, Author "John" "Mueller", Author "Irem" "Tumer"]
+  [walkingshaw, Author "Paul" "Strauss", erwig, Author "John" "Mueller", Author "Irem" "Tumer"]
   "A Formal Representation of Software-Hardware System Design"
   2009
-  detc
-  (Pages 1387 1398)
+  `onPages` Pages 1387 1398
+  @@ detc
 
-jfp09 = appeared
+jfp09 = appeared Journal
   "jfp09-hagl"
-  [me,martin]
+  [walkingshaw,erwig]
   "A Domain-Specific Language for Experimental Game Theory"
   2009
-  (jfp (Just 19) Nothing)
-  (Pages 645 661)
+  `onPages` Pages 645 661
+  @@ jfp `setVolume` 19
   
-dsl09a = appeared
+dsl09a = appeared Conference
   "dsl09-explaining-probability"
-  [martin,me]
+  [erwig,walkingshaw]
   "A DSL for Explaining Probabilistic Reasoning"
   2009
-  (dsl (Just (lncs 5658)))
-  (Pages 335 359)
+  `onPages` Pages 335 359
   `withNote` "Best paper"
+  @@ dsl `setSeries` lncs 5658
 
-dsl09b = appeared
+dsl09b = appeared Conference
   "dsl09-hagl-expressiveness"
-  [me,martin]
+  [walkingshaw,erwig]
   "Varying Domain Representations in Hagl -- Extending the Expressiveness of a DSL for Experimental Game Theory"
   2009
-  (dsl (Just (lncs 5658)))
-  (Pages 310 334)
+  `onPages` Pages 310 334
+  @@ dsl `setSeries` lncs 5658
 
 
 -- ** 2008
 
-vlhcc08 = appeared
+vlhcc08 = appeared Conference
   "vlhcc08-explaining-strategies"
-  [martin,me]
+  [erwig,walkingshaw]
   "A Visual Language for Representing and Explaining Strategies in Game Theory"
   2008
-  vlhcc
-  (Pages 101 108)
+  `onPages` Pages 101 108
+  @@ vlhcc
 
-vlhcc08dc = appeared
+vlhcc08dc = appeared Consortium
   "vlhcc09dc-explanation-oriented-languages"
-  [me]
+  [walkingshaw]
   "Designing Explanation-Oriented Languages"
   2008
-  vlhccdc
-  (Pages 274 275)
+  `onPages` Pages 274 275
+  @@ vlhccdc
