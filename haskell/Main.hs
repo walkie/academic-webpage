a4681422be7f1f3bb86f10bf025414eeddd40351
Eric Walkingshaw <eric@walkingshaw.net>
2013-09-22 00:12:57 +0200

    Add new Main module.

---
+++ haskell/Main.hs
@@ -0,0 +1,8 @@
+
+module Main where
+
+import Hakyll
+
+import WebPage.Generate
+
+main = hakyllWith config rules
---
+++ haskell/WebPage/Generate.hs
@@ -0,0 +1,8 @@
+
+module WebPage.Generate (
+  module WebPage.Generate.Base,
+  module WebPage.Generate.Rules,
+) where
+
+import WebPage.Generate.Base
+import WebPage.Generate.Rules
